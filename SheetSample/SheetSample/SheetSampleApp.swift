ece6a84b112e08cde8091c8921e93aac4fe187e4
Kuriyama <paprapapra@gmail.com>
2022-01-16 11:49:04 +0900

    Add project files.

---
+++ SheetSample/SheetSample/SheetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetSampleApp.swift
+//  SheetSample
+//
+//  Created by kotetuco on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct SheetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
