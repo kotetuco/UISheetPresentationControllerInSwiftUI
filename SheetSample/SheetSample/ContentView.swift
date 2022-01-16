e273ef55eedc43500072151d059346b47ea2b954
Kuriyama <paprapapra@gmail.com>
2022-01-16 15:39:15 +0900

    Implemented: displaying UISheetPresentationController in SwiftUI and  DatePicker

--- SheetSample/SheetSample/ContentView.swift
+++ SheetSample/SheetSample/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSheet: Bool = false
+    @State private var uiImage: UIImage?
+    @State private var date = Date()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(spacing: 10) {
+                if let uiImage = uiImage {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .padding()
+                } else {
+                    Text(dateTimeText)
+                }
+
+                Button {
+                    showSheet.toggle()
+                } label: {
+                    Text("Show a half sheet.")
+                }
+                .buttonStyle(.borderedProminent)
+                .sheetPresentation(isPresented: $showSheet, sheetView: {
+//                    ImagePickerController { uiImage in
+//                        self.uiImage = uiImage
+//                    }
+//                    .edgesIgnoringSafeArea(.bottom)
+                    DatePicker("Select date and time.", selection: $date)
+                        .datePickerStyle(.graphical)
+                        .environment(\.locale, Locale(identifier: Locale.preferredLanguages.first!))
+                }, onDismiss: nil)
+                .padding()
+            }
+            .navigationTitle("Half Sheet Sample")
+        }
+    }
+
+    private var dateTimeText: String {
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        return "\(dateComponents.year!)-\(dateComponents.month!)-\(dateComponents.day!) \(dateComponents.hour!):\(dateComponents.minute!)"
     }
 }
 
---
+++ SheetSample/SheetSample/SheetPresentationController.swift
@@ -0,0 +1,105 @@
+//
+//  SheetPresentationController.swift
+//  SheetSample
+//
+//  Created by kotetuco on 2022/01/16.
+//
+
+import PhotosUI
+import SwiftUI
+import UIKit
+
+public extension View {
+    func sheetPresentation<SheetView: View>(isPresented: Binding<Bool>,
+                                            @ViewBuilder sheetView: @escaping () -> SheetView,
+                                            onDismiss: (() -> ())? = nil) -> some View {
+        background {
+            SheetPresentationController(isPresented: isPresented, sheetView: sheetView(), onDismiss: onDismiss)
+        }
+    }
+}
+
+/// ImagePickerControllerは使わないでください(UIHostingControllerと合わせて使うことで動作がカクついてスクロールが使い物にならなくなります)
+public struct SheetPresentationController<SheetView: View>: UIViewControllerRepresentable {
+    public typealias UIViewControllerType = UIViewController
+
+    @Binding private var isPresented: Bool
+    private var sheetView: SheetView
+    private var onDismiss: (() -> ())?
+
+    init(isPresented: Binding<Bool>, sheetView: SheetView, onDismiss: (() -> ())?) {
+        self._isPresented = isPresented
+        self.sheetView = sheetView
+        self.onDismiss = onDismiss
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    public func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .clear
+        return viewController
+    }
+
+    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        if isPresented {
+            if uiViewController.presentedViewController == nil {
+                let sheetViewController = SheetHostingViewController(rootView: sheetView)
+                sheetViewController.sheetPresentationController?.delegate = context.coordinator
+                sheetViewController.delegate = context.coordinator
+                if let sheetPresentationController = sheetViewController.sheetPresentationController {
+                    sheetPresentationController.detents = [.medium(), .large()]
+                    sheetPresentationController.prefersGrabberVisible = true
+                    sheetPresentationController.prefersScrollingExpandsWhenScrolledToEdge = false
+                    sheetPresentationController.preferredCornerRadius = 20
+                }
+                uiViewController.present(sheetViewController, animated: true)
+            }
+        } else {
+            uiViewController.dismiss(animated: true)
+        }
+    }
+
+    public final class Coordinator: NSObject, UISheetPresentationControllerDelegate, SheetHostingViewControllerDelegate {
+        var parent: SheetPresentationController
+
+        init(parent: SheetPresentationController) {
+            self.parent = parent
+        }
+
+        public func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+            if self.parent.isPresented {
+                self.parent.isPresented = false
+                if let onDismiss = self.parent.onDismiss {
+                    onDismiss()
+                }
+            }
+        }
+
+        internal func sheetViewControllerDidDisappear<Content>(_ sheetViewController: SheetHostingViewController<Content>) where Content : View {
+            self.parent.isPresented = false
+            if let onDismiss = self.parent.onDismiss {
+                onDismiss()
+            }
+        }
+    }
+}
+
+protocol SheetHostingViewControllerDelegate: AnyObject {
+    func sheetViewControllerDidDisappear<Content: View>(_ sheetViewController: SheetHostingViewController<Content>)
+}
+
+final class SheetHostingViewController<Content: View>: UIHostingController<Content> {
+    weak var delegate: SheetHostingViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        delegate?.sheetViewControllerDidDisappear(self)
+    }
+}
